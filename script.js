548eaba1bb9aac51c21d6aa9cbedceef90872ef3
Deepakkumar121998 <130857964+Deepakkumar121998@users.noreply.github.com>
2023-06-15 10:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+<table>
+		<tr>
+			<td>id</td>
+			<td>Background</td>
+			<td>Border-width</td>
+			<td>Border-color</td>
+		</tr>
+		<tr>
+			<td>yellow</td>
+			<td>yellow</td>
+			<td>1px</td>
+			<td>green</td>
+		</tr>
+		<tr>
+			<td>red</td>
+			<td>red</td>
+			<td>1px</td>
+			<td>black</td>
+		</tr>
+		<tr>
+			<td>green</td>
+			<td>green</td>
+			<td>1px</td>
+			<td>yellow</td>
+		</tr>
+		
+	</table>
